dbc7e4a95fb9835882e4a7fca697919084bb2f52
MoniqueSukerman <nique_sukerman@hotmail.com>
2022-07-28 21:39:04 -0300

    Salvando alterações nos arquivos das aulas de 01 à 03

--- operacoes.php
+++ operacoes.php
@@ -4,3 +4,12 @@ $idade = 26;
 $idadeDaqui10Anos = $idade + 10;
 
 echo $idadeDaqui10Anos;
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+
+$doisAoCubo = 2 ** 3;
+
+$restoDaDivisao = 10 % 3;
